6d9e235e659b2ce6dd2119018c7062f5d988c289
uid38717 <anuj.shah@continental-corporation.com>
2019-10-26 13:08:30 +0530

    added new path to trained model

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1FTGV-5eOBrdGDUv2bh2x2o-zoYsJ0UiA'
 export_file_name = 'export.pkl'
 
 classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
